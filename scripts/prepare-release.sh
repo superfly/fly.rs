1e7e4ed9ba5bb961d08e53950e191d5e11bbeb08
Jerome Gravel-Niquet <jeromegn@gmail.com>
2018-12-19 16:22:58 -0500

    try with better s3 permissions and upload dir

--- scripts/prepare-release.sh
+++ scripts/prepare-release.sh
@@ -3,3 +3,5 @@ cp target/release/dns fly-dns
 strip target/release/distributed-fly
 cp target/release/distributed-fly fly-dist
 tar czf $RELEASE_FILENAME fly-dns fly-dist
+mkdir -p release
+mv $RELEASE_FILENAME release/
