2e6d5dcb55c8ebfdeff3de428394a83bfe480202
Jerome Gravel-Niquet <jeromegn@gmail.com>
2019-01-08 17:42:04 -0500

    remove fbs from build script

--- build.rs
+++ build.rs
@@ -4,10 +4,4 @@ fn main() {
   let output = Command::new("git").args(&["rev-parse", "HEAD"]).output().unwrap();
   let git_hash = String::from_utf8(output.stdout).unwrap();
   println!("cargo:rustc-env=GIT_HASH={}", git_hash[..7].to_string());
-
-  println!("cargo:rerun-if-changed=msg.fbs");
-  let status = Command::new("./scripts/fbs.sh")
-    .status()
-    .expect("failed to generated flatbuffer messages");
-  assert!(status.success());
 }
