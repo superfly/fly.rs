ff59f14b2a069d9172dc55debc9f2f1498b5287f
Michael Dwan <m@dwan.io>
2019-01-09 13:18:23 -0600

    Move acme_store back to runtime

--- src/ops/acme.rs
+++ src/ops/acme.rs
@@ -1,34 +1,23 @@
-use crate::acme_store::AcmeStore;
 use crate::msg;
-use crate::redis_acme::RedisAcmeStore;
 use crate::runtime::{JsRuntime, Op};
-use crate::settings::{AcmeStoreConfig, SETTINGS};
 use crate::utils::*;
 use flatbuffers::FlatBufferBuilder;
 use futures::Future;
 use libfly::*;
 
-lazy_static! {
-    static ref ACME_STORE: Option<Box<AcmeStore + 'static + Send + Sync>> =
-        match &SETTINGS.read().unwrap().acme_store {
-            Some(ref store) => match store {
-                AcmeStoreConfig::Redis(conf) => Some(Box::new(RedisAcmeStore::new(&conf))),
-            },
-            None => None,
-        };
-}
-
-pub fn op_get_challenge(_ptr: JsRuntime, base: &msg::Base, _raw: fly_buf) -> Box<Op> {
+pub fn op_get_challenge(ptr: JsRuntime, base: &msg::Base, _raw: fly_buf) -> Box<Op> {
     let cmd_id = base.cmd_id();
     let msg = base.msg_as_acme_get_challenge().unwrap();
     let hostname = msg.hostname().unwrap().to_string();
     let token = msg.token().unwrap().to_string();
 
-    if ACME_STORE.is_none() {
+    let acme_store = &ptr.to_runtime().acme_store;
+
+    if acme_store.is_none() {
         return Box::new(odd_future("No acme store configured".to_string().into()));
     }
 
-    let acme_store = ACME_STORE.as_ref().unwrap();
+    let acme_store = acme_store.as_ref().unwrap();
 
     Box::new(
         acme_store
