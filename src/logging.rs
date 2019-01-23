a2187cd26c1bec544304f535b8277f89ac7ea4df
Michael Dwan <m@dwan.io>
2019-01-22 19:35:49 -0600

    Configure runtime & app logs with ENV

--- src/logging.rs
+++ src/logging.rs
@@ -1,15 +1,50 @@
-use slog::{o, Drain, Logger};
+use slog::{o, Drain, Level, Logger};
+use slog_async::Async;
 use slog_scope::GlobalLoggerGuard;
+use slog_term::term_full;
+use std::env;
 
 pub fn configure() -> (GlobalLoggerGuard, Logger) {
-  let decorator = slog_term::TermDecorator::new().build();
-  let drain = slog_term::FullFormat::new(decorator).build().fuse();
-  let drain = slog_async::Async::new(drain).build().fuse();
-  let logger = slog::Logger::root(drain, o!("build" => crate::build_number()));
-  let runtime_logger = logger.new(o!("source" => "rt"));
-  let app_logger = logger.new(o!("source" => "app"));
+  let runtime_logger = Logger::root(
+    Async::default(
+      term_full()
+        .filter_level(log_level_from_env("FLY_LOG_LEVEL", Level::Warning))
+        .fuse(),
+    )
+    .fuse(),
+    o!(
+      "build" => crate::build_number(),
+      "source" => "rt",
+    ),
+  );
+
+  let app_logger = Logger::root(
+    Async::default(
+      term_full()
+        .filter_level(log_level_from_env("LOG_LEVEL", Level::Debug))
+        .fuse(),
+    )
+    .fuse(),
+    o!(
+      "build" => crate::build_number(),
+      "source" => "app",
+    ),
+  );
+
   let _guard = slog_scope::set_global_logger(runtime_logger);
   slog_stdlog::init().unwrap();
 
   return (_guard, app_logger);
+}
+
+pub fn log_level_from_env(name: &str, default: Level) -> Level {
+  match env::var(name).unwrap_or_default().as_ref() {
+    "TRACE" => Level::Trace,
+    "DEBUG" => Level::Debug,
+    "INFO" => Level::Info,
+    "WARN" => Level::Warning,
+    "WARNING" => Level::Warning,
+    "ERROR" => Level::Error,
+    _ => default,
+  }
 }
