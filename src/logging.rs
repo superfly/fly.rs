bcdca8a4e935bd059d505d618d2e81ec739b2247
Michael Dwan <m@dwan.io>
2019-01-23 12:52:39 -0600

    Share stdout drain between runtime and app loggers

--- src/logging.rs
+++ src/logging.rs
@@ -5,46 +5,42 @@ use slog_term::term_full;
 use std::env;
 
 pub fn configure() -> (GlobalLoggerGuard, Logger) {
-  let runtime_logger = Logger::root(
-    Async::default(
-      term_full()
-        .filter_level(log_level_from_env("FLY_LOG_LEVEL", Level::Warning))
-        .fuse(),
-    )
-    .fuse(),
-    o!(
-      "build" => crate::build_number(),
-      "source" => "rt",
-    ),
-  );
+    let root_logger = Logger::root(
+        Async::default(term_full().fuse()).fuse(),
+        o!(
+          "build" => crate::build_number(),
+        ),
+    );
 
-  let app_logger = Logger::root(
-    Async::default(
-      term_full()
-        .filter_level(log_level_from_env("LOG_LEVEL", Level::Debug))
-        .fuse(),
-    )
-    .fuse(),
-    o!(
-      "build" => crate::build_number(),
-      "source" => "app",
-    ),
-  );
+    let runtime_logger = Logger::root(
+        root_logger
+            .new(o!())
+            .filter_level(log_level_from_env("FLY_LOG_LEVEL", Level::Warning))
+            .fuse(),
+        o!("source" => "rt"),
+    );
 
-  let _guard = slog_scope::set_global_logger(runtime_logger);
-  slog_stdlog::init().unwrap();
+    let app_logger = Logger::root(
+        root_logger
+            .filter_level(log_level_from_env("LOG_LEVEL", Level::Debug))
+            .fuse(),
+        o!("source" => "app"),
+    );
 
-  return (_guard, app_logger);
+    let _guard = slog_scope::set_global_logger(runtime_logger);
+    slog_stdlog::init().unwrap();
+
+    return (_guard, app_logger);
 }
 
 pub fn log_level_from_env(name: &str, default: Level) -> Level {
-  match env::var(name).unwrap_or_default().as_ref() {
-    "TRACE" => Level::Trace,
-    "DEBUG" => Level::Debug,
-    "INFO" => Level::Info,
-    "WARN" => Level::Warning,
-    "WARNING" => Level::Warning,
-    "ERROR" => Level::Error,
-    _ => default,
-  }
+    match env::var(name).unwrap_or_default().as_ref() {
+        "TRACE" => Level::Trace,
+        "DEBUG" => Level::Debug,
+        "INFO" => Level::Info,
+        "WARN" => Level::Warning,
+        "WARNING" => Level::Warning,
+        "ERROR" => Level::Error,
+        _ => default,
+    }
 }
