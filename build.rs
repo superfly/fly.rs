9c884129778da28cb1a272a5aad72dc40e571a97
Jerome Gravel-Niquet <jeromegn@gmail.com>
2019-01-30 14:28:47 -0500

    get a short git sha

--- build.rs
+++ build.rs
@@ -1,5 +1,5 @@
 use std::env;
 
 fn main() {
-    println!("cargo:rustc-env=BUILD_VERSION={}", env::var("BUILD_VERSION").unwrap_or(env::var("TRAVIS_COMMIT").unwrap_or("unknown".to_string())));
+    println!("cargo:rustc-env=BUILD_VERSION={}", env::var("BUILD_VERSION").unwrap_or(env::var("TRAVIS_COMMIT").unwrap_or("unknown".to_string()).chars().take(7).collect()));
 }
